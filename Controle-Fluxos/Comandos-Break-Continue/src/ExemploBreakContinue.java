a9cc96fc215de00a22452c3bd8adf7237421a322
Edsonfrs <edsonfrs@gmail.com>
2023-07-31 13:15:06 -0300

    Estruturas While e Do While

--- Controle-Fluxos/Comandos-Break-Continue/src/ExemploBreakContinue.java
+++ Controle-Fluxos/Comandos-Break-Continue/src/ExemploBreakContinue.java
@@ -2,7 +2,7 @@ public class ExemploBreakContinue {
     public static void main(String[] args) throws Exception {
         
         for (int numero = 1; numero <= 10; numero++) {
-            if (numero == 5) {
+            if (numero == 8) {
                 break;
             }
             System.out.println(numero);
